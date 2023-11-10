c72252d3d2a0b224c4b10847ee1543571bc7953a
Brennan0 <51968218+Brennan0@users.noreply.github.com>
2023-11-10 12:41:47 -0800

    grid fix

--- src/DragDrop.js
+++ src/DragDrop.js
@@ -9,29 +9,25 @@ export default function DragDrop() {
         <>
         <div style={{width: "80%", height: "100%"}}>
             <div className="year">
-            <Quarter sectionTitle={"Fall Fresh"}>
-            </Quarter >
+            <Quarter sectionTitle={"Fall Fresh"}/>
             <Quarter sectionTitle={"Winter Fresh"}/>
             <Quarter sectionTitle={"Spring Fresh"}/>
             <Quarter sectionTitle={"Summer Fresh"}/>
             </div>
             <div className="year">
-            <Quarter sectionTitle={"Fall Soph"}>
-            </Quarter >
+            <Quarter sectionTitle={"Fall Soph"}/>
             <Quarter sectionTitle={"Winter Soph"}/>
             <Quarter sectionTitle={"Spring Soph"}/>
             <Quarter sectionTitle={"Summer Soph"}/>
             </div>
             <div className="year">
-            <Quarter sectionTitle={"Fall Junior"}>
-            </Quarter >
+            <Quarter sectionTitle={"Fall Junior"}/>
             <Quarter sectionTitle={"Winter Junior"}/>
             <Quarter sectionTitle={"Spring Junior"}/>
             <Quarter sectionTitle={"Summer Junior"}/>
             </div>
             <div className="year">
-            <Quarter sectionTitle={"Fall Senior"}>
-            </Quarter >
+            <Quarter sectionTitle={"Fall Senior"}/>
             <Quarter sectionTitle={"Winter Senior"}/>
             <Quarter sectionTitle={"Spring Senior"}/>
             <Quarter sectionTitle={"Summer Senior"}/>
