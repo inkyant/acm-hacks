3fbc20cdfad3f48905ada1bb40d6536a0c6cbd94
Brennan0 <51968218+Brennan0@users.noreply.github.com>
2023-11-10 12:15:53 -0800

    add grid format

--- src/DragDrop.js
+++ src/DragDrop.js
@@ -7,12 +7,35 @@ export default function DragDrop() {
 
     return (
         <>
-        <div style={{width: "80%", height: "100%", display: "flex"}}>
-            <Quarter sectionTitle={"Fall"}>
+        <div style={{width: "80%", height: "100%"}}>
+            <div className="year">
+            <Quarter sectionTitle={"Fall Fresh"}>
             </Quarter >
-            <Quarter sectionTitle={"Winter"}/>
-            <Quarter sectionTitle={"Spring"}/>
-            <Quarter sectionTitle={"Summer"}/>
+            <Quarter sectionTitle={"Winter Fresh"}/>
+            <Quarter sectionTitle={"Spring Fresh"}/>
+            <Quarter sectionTitle={"Summer Fresh"}/>
+            </div>
+            <div className="year">
+            <Quarter sectionTitle={"Fall Soph"}>
+            </Quarter >
+            <Quarter sectionTitle={"Winter Soph"}/>
+            <Quarter sectionTitle={"Spring Soph"}/>
+            <Quarter sectionTitle={"Summer Soph"}/>
+            </div>
+            <div className="year">
+            <Quarter sectionTitle={"Fall Junior"}>
+            </Quarter >
+            <Quarter sectionTitle={"Winter Junior"}/>
+            <Quarter sectionTitle={"Spring Junior"}/>
+            <Quarter sectionTitle={"Summer Junior"}/>
+            </div>
+            <div className="year">
+            <Quarter sectionTitle={"Fall Senior"}>
+            </Quarter >
+            <Quarter sectionTitle={"Winter Senior"}/>
+            <Quarter sectionTitle={"Spring Senior"}/>
+            <Quarter sectionTitle={"Summer Senior"}/>
+            </div>
         </div>
         <CardBank startCards={classList} />
         </>
