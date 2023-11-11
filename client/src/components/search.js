d80419b142386713530688d3a12cb8553c9475b7
Anthony Furman <80181074+inkyant@users.noreply.github.com>
2023-11-11 00:51:33 -0800

    simplify search.js

--- client/src/components/search.js
+++ client/src/components/search.js
@@ -1,44 +1,39 @@
-import React from 'react'
+import React, { useRef } from 'react'
 import './search.css'
 import { useState } from "react";
 import { useEffect } from "react";
 
 
-export default function Search(){
-    useEffect(()=>{
-        fetch('https://jsonplaceholder.typicode.com/users') /* url in '' */
-        .then(res => res.json())
-        .then(data => {
-            console.log(data);
-            setFilterData(data);
-        })
-        .catch(err => console.long(err));
-    },[])
-        const [data, setData] = useState([])
-        const [filterData, setFilterData] = useState([])
-        const handleFilter = (value) => {
-            const res = filterData.filter(f => f.name.toLowerCase().includes(value))
-            setData(res);
-            if(value === "") {
-                setData([])
-            }
+export default function Search() {
+
+    useEffect(() => {
+        
+    }, [])
+
+    const ref = useRef(null)
+
+    const handleKeyDown = ({ key }) => {
+        if (key === 'Enter') {
+            
+            let subject = ref.current.value.match(/[a-zA-Z]{2,3}/g)
+            let number = ref.current.value.match(/\d{1,3}.?/g)
+
+            console.log('http://localhost:5000/courses/'+subject+'/'+number)
+
+            fetch('http://localhost:5000/courses/cse/12')
+            .then(res => res.json())
+            .then(data => {
+                console.log(data);
+            })
+            .catch(err => console.log(err));
         }
-    return(
+    }
+
+    return (
         <div className = "search-top">
             <div className = 'search'>
-                <input type="text" placeholder = "Search Here" onChange={ e => handleFilter(e.target.value)}/>
-            </div>
-            <div className = "search-result">
-                {data.map((d, i) => (
-                <div key={i}>
-                    {d.name}
-                </div>
-                ))}
+                <input ref={ref} type="text" placeholder="Search Here" onKeyDown={handleKeyDown}/>
             </div>
         </div>
-
-
-
     )
-
 }
