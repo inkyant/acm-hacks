d38f49b4ee9f72a55f334156c42ef3a259efdd20
Anthony Furman <80181074+inkyant@users.noreply.github.com>
2023-11-10 20:29:51 -0800

    elevate react state

--- src/DragDrop.js
+++ src/DragDrop.js
@@ -1,30 +1,52 @@
 
 import CardBank from "./components/cardBank";
 import Quarter from "./components/quarter";
-import { classList } from "./App";
+import { classList, getClassById } from "./App";
+import { useState } from "react";
 
 export default function DragDrop() {
+
+    const [classes, setClasses] = useState([classList, [[], [], [], []], [[], [], [], []], [[], [], [], []], [[], [], [], []]])
 
     return (
         <>
             <div style={{ width: "80%", height: "90%" }}>
-                <Year title="Frosh" />
-                <Year title="Soph" />
-                <Year title="Junior" />
-                <Year title="Senior" />
+                <Year year={1} yearClasses={classes[1]} setClasses={setClasses} />
+                <Year year={2} yearClasses={classes[2]} setClasses={setClasses} />
+                <Year year={3} yearClasses={classes[3]} setClasses={setClasses} />
+                <Year year={4} yearClasses={classes[4]} setClasses={setClasses} />
             </div>
-            <CardBank startCards={classList} />
+            <CardBank startCards={classes[0]} />
         </>
     );
 }
 
-function Year({ title }) {
+function Year({ year, yearClasses, setClasses }) {
+
+    const addClass = (i, { id, remove }) => {
+        console.log(year, id)
+        remove()
+        setClasses((oldClasses) => {
+            const newClasses = JSON.parse(JSON.stringify(oldClasses))
+            newClasses[year][i].push(getClassById(id))
+            return newClasses
+        })
+    }
+
+    const removeClass = (quarter, id) => {
+        setClasses(oldClasses => {
+            let newClasses = [...oldClasses]
+            newClasses[year][quarter] = newClasses[year][quarter].filter(c => c.id !== id)
+            return newClasses
+        })
+    }
+
     return (
         <div className="year">
-            <Quarter sectionTitle={"Fall " + title} />
-            <Quarter sectionTitle={"Winter " + title} />
-            <Quarter sectionTitle={"Spring " + title} />
-            <Quarter sectionTitle={"Summer " + title} />
+            <Quarter sectionTitle={"Fall " + year}   classes={yearClasses[0]} addClass={(item) => addClass(0, item)} removeClass={(id) => removeClass(0, id)} />
+            <Quarter sectionTitle={"Winter " + year} classes={yearClasses[1]} addClass={(item) => addClass(1, item)} removeClass={(id) => removeClass(1, id)} />
+            <Quarter sectionTitle={"Spring " + year} classes={yearClasses[2]} addClass={(item) => addClass(2, item)} removeClass={(id) => removeClass(2, id)} />
+            <Quarter sectionTitle={"Summer " + year} classes={yearClasses[3]} addClass={(item) => addClass(3, item)} removeClass={(id) => removeClass(3, id)} />
         </div>
     );
 }
--- src/components/quarter.js
+++ src/components/quarter.js
@@ -6,9 +6,7 @@ import Card from "./card";
 import './quarter.css'
 
 
-export default function Quarter({ sectionTitle }) {
-
-    const [classes, setClasses] = useState([])
+export default function Quarter({ sectionTitle, classes, addClass, removeClass}) {
 
     const [{ isOver, canDrop }, drop] = useDrop(() => ({
         accept: "class",
@@ -20,43 +18,19 @@ export default function Quarter({ sectionTitle }) {
         })
     }));
 
-    const addClass = ({ id, remove }) => {
-        remove()
-        setClasses((sched) => [...sched, getClassById(id)])
-    }
-
     const canAddClass = ({ id }) => {
-        // really dumb way to access current state, use setState...
-        let x = false
-        setClasses(classes => {
-            // can add class if not already added, and
-            x = !classes.map((c) => c.id).includes(id)
-            // if <= 19 credits, classes.reduce(...) sums the credits
-            x &= classes.reduce((partialSum, a) => partialSum + a.credits, 0) + getClassById(id).credits <= 19
-            return classes
-        })
+        // can add class if not already added, and
+        let x = !classes.map((c) => c.id).includes(id)
+        // if <= 19 credits; classes.reduce(...) sums the credits
+        x &= classes.reduce((partialSum, a) => partialSum + a.credits, 0) + getClassById(id).credits <= 19
         return x
-    }
-
-    const removeCard = id => {
-        setClasses(oldCards => { return oldCards.filter(c => c.id != id) })
-    }
-
-    const checkPrereq = (classesTaken, prereq) => {
-        return prereq.every((oneReq) => {
-            return oneReq.some((elem) => {
-                return classesTaken.includes(elem)
-            })
-        })
     }
 
     return (
         <div className="quarter" style={{ opacity: isOver && canDrop ? 0.5 : 1 }} ref={drop}>
             <div className="sectionTitle">{sectionTitle}</div>
             {classes.map((card) => {
-                // let prevClass = classes.map((e) => e.title) + getPrevClasses()
-                // let c = checkPrereq(prevClass, card.prereq)
-                return (<Card cardData={card} key={card.id} preqCleared={true} removeCard={() => removeCard(card.id)} />);
+                return (<Card cardData={card} key={card.id} preqCleared={true} removeCard={() => removeClass(card.id)} />);
             })}
         </div>
     );
